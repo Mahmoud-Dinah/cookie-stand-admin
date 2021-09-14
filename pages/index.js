0cb5980b3a845a4d03499fc424b68cfcec2653d8
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-09-14 05:09:06 +0300

    edit lab 37

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,61 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import React,{useState} from 'react'
+
 
 export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>cookie-stand-admin</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [cookiesInformations,setCookIesinfo] = useState([])
+  function cookieInfoHandler(event) {
+    event.preventDefault();
+    const cookiesInfo = {
+      location: event.target.location.value,
+      MinCustPerHour: event.target.mincustperhour.value,
+      MaxcustPerHour: event.target.maxcustperhour.value,
+      AverageCooki: event.target.averagecooki.value
+    }
+    setCookIesinfo(info => [cookiesInfo])
+  }
 
-      <main>
-        <header className="flex items-center justify-between p-4 bg-green-500 text-green-50">
-          <h1 className="text-4xl">cookie-stand-admin</h1>
-          <p>some text</p>
-        </header>
-        <form className="m-8 p-4 bg-green-200  rounded-md"> 
-        
-        <h2 className="text-3xl">cookie-stand-admin</h2>
+  function getLatestReply() {
+    if(cookiesInformations.length>=1){
+      return JSON.stringify(cookiesInformations[cookiesInformations.length-1]
+        )
+    } else{
+      return "Nothing added"
+    }
+  }
+  return (
+
+    <div >
+    <Head>
+      <title>Cookie Stand Admin</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main >
+    <header className = "flex items-start justify-between p-4 bg-green-500 text-black-500"><h1 className="text-xl">Cookie Stand Admin</h1></header>
+    <form className="m-8 p-4 bg-green-200  rounded-md" onSubmit={cookieInfoHandler}>
+        <h2 className ="text-center text-xl">Create Cookie Stand </h2>
         <div className="flex items-stretch">
         <label className=" m-3"for="location">Location</label>
         <input name="location"className= " bg-gray-100 m-3 w-2/3" />
-          </div>
-        <table className="mx-auto w-1/2 my-8">
-          <thead>
-            <tr>
-              <th className="border border-gray-70">Max Customer per Hour</th>
-              <th className="border border-gray-70">Min Customer per Hour</th>
-              <th className="border border-gray-70">Average Cookie per sale</th>
-            </tr>
-          </thead>
-          <tbody>
-                <td className="pl-2 border border-gray-700">1</td>
-                <td className="pl-2 border border-gray-700">2</td>
-                <td className="pl-2 border border-gray-700">3</td>
-          </tbody>
-        </table>
+        </div>
+        <div className="flex items-stretch">
+        <label className=" m-3" for="mincustperhour">Min customers per hour</label>
+        <input name="mincustperhour"/>
+        <label className=" m-3" for="maxcustperhour">Max customers per hour</label>
+        <input name="maxcustperhour"/>
+        <label className=" m-3" for="averagecooki">Average cookies per sale</label>
+        <input name="averagecooki"/>
         <button className="px-2 p-1 m-4 bg-green-500 text-black-50" >Create</button>
-        </form>
-        <footer className="p-4 mt-8 bg-green-500 text-green-50">
-            <p>&copy; 2021</p>
-            
-        </footer>
-      </main>
-    </div>
-  )
+        </div>
+    </form>
+  <h2 className ="text-center text-xl">{getLatestReply()}</h2>
+    </main>
+
+    <footer className = "p-4 mt-8 bg-green-500 text-black-50" >
+    <p>&copy; 2021</p>
+    </footer>
+  </div>
+)
 }
