23331e074cdf7e151c256390ff44be9fd620f7b1
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-09-15 05:23:44 +0300

    finsih lab

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -3,7 +3,7 @@ function formHandler(event){
     event.preventDefault()
     const formInfo = {
         location:event.target.location.value,
-        hour_sales: [10, 20, 30, 30, 40, 50, 60, 70, 80, 90, 100, 20, 30, 45]
+        hour_sales: [10, 20, 30, 40, 40, 50, 60, 70, 80, 90, 100, 20, 30, 45]
 
     }
     cookieInfoHandler(formInfo)
